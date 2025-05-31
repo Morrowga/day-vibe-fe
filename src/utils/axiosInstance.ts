c980e375afc5feaf12121dc8142934c2d533869a
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-01 00:27:56 +0700

    env setupg

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || '/api'
+
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.W10.Mwnyln9vH_mz9xoWX_8mmq6tCHrno0fjqJeNU3acH61Q5rnmu_FQtp_5PNGx1gsMM-A3rQbE5JY7Gmfl19Karv39697-_cWoQvoPiXg94c34ZgH9sJqkI3UCYB7vxXcPfsvuO04OMMzVVazxskWxk1k9MBWlgRbiN6e5yba_QBm_wzUqzHNliZBA0ruhhmFJcXRDtVHgmLy1NijqpdJx8mxwX4xQLVCHodTrt7s22Yk1BnhxPwmQPwxuC-gg74C3cmddFn2nEOTF2BDm6gAcc7nLD9yR2D8uEYVKvSLbAdQUVIle6K1qXGLdqF4S67ZWQByUGyyxhP9gNBIgUuZ-EA'
