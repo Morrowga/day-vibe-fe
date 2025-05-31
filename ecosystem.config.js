3be9f346f4d7661aac5c0a6fab51ccd6d063dab3
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-01 01:20:00 +0700

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'nextjs-frontend',
-      script: './node_modules/.bin/next',
+      script: 'npm',
       args: 'start',
       cwd: '/var/www/frontend',
       env: {
         PORT: 3000,
-        NODE_ENV: 'production',
-        NEXT_PUBLIC_API_URL: 'http://13.229.70.118/api'
-      },
-      instances: 1,
-      exec_mode: 'fork',
-      watch: false,
-      max_memory_restart: '1G'
+        NODE_ENV: 'production'
+      }
     }
   ]
 }
