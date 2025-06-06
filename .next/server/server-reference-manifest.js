25e0aaac73967e38a8f6c530bc8e0b0c106f7dbc
thihaaung-plusimpact <48879518+Morrowga@users.noreply.github.com>
2025-06-06 18:05:03 +0700

    build and env

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"9G4qK7kuHIxzKyS5SKpXmcqmPuAPEOYNap3hXXF8Ggs=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jIzn2wcUKvGPGayITPkZtjxXvlR5E43asoam96JW5OQ=\"}"
